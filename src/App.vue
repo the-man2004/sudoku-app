b9c1fb33dd6ee6e5906c90303c371eddc3436699
the-man2004 <heinrichhb111@gmail.com>
2024-01-31 14:05:58 -0800

    Added a gradient background & some reset styles.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
   <h1>Hello world!</h1>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
+  min-height: 100vh;
+
+  background: linear-gradient(120deg, #b1b6e7, #dda5a5);
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
