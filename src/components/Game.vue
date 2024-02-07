d70ea8433eb766ae059569f8e6c1b3a4445056ef
the-man2004 <heinrichhb111@gmail.com>
2024-02-07 10:43:44 -0800

    Moved the numbad into it's own component.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -16,85 +16,14 @@
         {{ number === 0 ? "" : number }}
       </div>
     </div>
-    <div class="numpad">
-      <button @click="handleBtnClick(1)">
-        1
-        <span>{{
-          "1" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["1"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(2)">
-        2
-        <span>{{
-          "2" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["2"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(3)">
-        3
-        <span>{{
-          "3" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["3"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(4)">
-        4
-        <span>{{
-          "4" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["4"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(5)">
-        5
-        <span>{{
-          "5" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["5"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(6)">
-        6
-        <span>{{
-          "6" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["6"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(7)">
-        7
-        <span>{{
-          "7" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["7"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(8)">
-        8
-        <span>{{
-          "8" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["8"]
-            : 9
-        }}</span>
-      </button>
-      <button @click="handleBtnClick(9)">
-        9
-        <span>{{
-          "9" in puzzleStore.occurrencesOfNumber
-            ? 9 - puzzleStore.occurrencesOfNumber["9"]
-            : 9
-        }}</span>
-      </button>
-    </div>
+    <!-- Placeholder -->
+    <Numpad></Numpad>
   </div>
 </template>
 
 <script setup>
 import { usePuzzleStore } from "../store/index";
+import Numpad from "./Numpad.vue";
 
 const puzzleStore = usePuzzleStore();
 
@@ -139,38 +68,6 @@ const handleItemClick = (event) => {
       el.style.color = "white";
       el.style.background = "rgb(72, 72, 255)";
     });
-  }
-};
-
-const showError = (item) => {
-  const gameBoard = document.querySelector(".game-board");
-
-  gameBoard.style.background = "rgba(255, 0, 0, 0.2)";
-
-  item.style.background = "red";
-  item.style.border = "5px solid white";
-
-  setTimeout(() => {
-    removeStyling();
-  }, 1000);
-};
-
-const handleBtnClick = (num) => {
-  if (puzzleStore.selectedItem !== null) {
-    const item = document.getElementById(puzzleStore.selectedItem.dataset.id);
-    const id = puzzleStore.selectedItem.dataset.id;
-
-    if (puzzleStore.solutionArr[id] === num) {
-      console.log("Correct!");
-
-      puzzleStore.addPuzzlePiece(id, num);
-      puzzleStore.checkVictory();
-    } else {
-      console.log("Wrong!");
-
-      showError(item);
-      puzzleStore.increaseMistakes();
-    }
   }
 };
 </script>
@@ -251,43 +148,6 @@ const handleBtnClick = (num) => {
   user-select: none;
 
   transition: all 300ms ease;
-}
-
-/* Number buttons */
-.numpad {
-  margin: 15px 0;
-  display: grid;
-  grid-template-columns: repeat(9, 1fr);
-  gap: 5px;
-}
-
-.numpad button {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  padding: 5px 0;
-
-  font-size: 1.5rem;
-  font-weight: 600;
-
-  border-radius: 5px;
-
-  background: rgb(72, 72, 255);
-  color: white;
-
-  cursor: pointer;
-  transition: all 300ms ease;
-}
-
-.numpad button:hover,
-.numpad button:focus {
-  background: blue;
-}
-
-.numpad button span {
-  font-size: 0.6em;
-  font-weight: 500;
 }
 
 @media (min-width: 500px) {
---
+++ src/components/Numpad.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="numpad">
+    <button @click="handleBtnClick(1)">
+      1
+      <span>{{
+        "1" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["1"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(2)">
+      2
+      <span>{{
+        "2" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["2"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(3)">
+      3
+      <span>{{
+        "3" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["3"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(4)">
+      4
+      <span>{{
+        "4" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["4"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(5)">
+      5
+      <span>{{
+        "5" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["5"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(6)">
+      6
+      <span>{{
+        "6" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["6"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(7)">
+      7
+      <span>{{
+        "7" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["7"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(8)">
+      8
+      <span>{{
+        "8" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["8"]
+          : 9
+      }}</span>
+    </button>
+    <button @click="handleBtnClick(9)">
+      9
+      <span>{{
+        "9" in puzzleStore.occurrencesOfNumber
+          ? 9 - puzzleStore.occurrencesOfNumber["9"]
+          : 9
+      }}</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { usePuzzleStore } from "@/store";
+
+const puzzleStore = usePuzzleStore();
+
+const removeStyling = () => {
+  const gameBoard = document.querySelector(".game-board");
+  const items = [...document.querySelectorAll(".item")];
+
+  // Remove styling
+  gameBoard.style.background = "rgba(0, 0, 0, 0.3)";
+
+  items.forEach((el) => {
+    el.style.color = "black";
+    el.style.background = "white";
+    // el.style.border = "none";
+  });
+};
+
+const showError = (item) => {
+  const gameBoard = document.querySelector(".game-board");
+
+  gameBoard.style.background = "rgba(255, 0, 0, 0.2)";
+
+  item.style.background = "red";
+  item.style.border = "5px solid white";
+
+  setTimeout(() => {
+    removeStyling();
+  }, 1000);
+};
+
+const handleBtnClick = (num) => {
+  if (puzzleStore.selectedItem !== null) {
+    const item = document.getElementById(puzzleStore.selectedItem.dataset.id);
+    const id = puzzleStore.selectedItem.dataset.id;
+
+    if (puzzleStore.solutionArr[id] === num) {
+      console.log("Correct!");
+
+      puzzleStore.addPuzzlePiece(id, num);
+      puzzleStore.checkVictory();
+    } else {
+      console.log("Wrong!");
+
+      showError(item);
+      puzzleStore.increaseMistakes();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.numpad {
+  margin: 15px 0;
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  gap: 5px;
+}
+
+.numpad button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 5px 0;
+
+  font-size: 1.5rem;
+  font-weight: 600;
+
+  border-radius: 5px;
+
+  background: rgb(72, 72, 255);
+  color: white;
+
+  cursor: pointer;
+  transition: all 300ms ease;
+}
+
+.numpad button:hover,
+.numpad button:focus {
+  background: blue;
+}
+
+.numpad button span {
+  font-size: 0.6em;
+  font-weight: 500;
+}
+</style>
